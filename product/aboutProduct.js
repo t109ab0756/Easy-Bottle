7858112e5215ab584f17522b540e3844d71d8447
謝 <151013528+t109ab0756@users.noreply.github.com>
2023-11-26 17:11:54 +0800

    Update aboutProduct.js

--- product/aboutProduct.js
+++ product/aboutProduct.js
@@ -1,15 +1,18 @@
 let header = document.querySelector("header");
+let Imag = document.querySelectorAll("header div.logo img");
 let headerAnchor = document.querySelectorAll("header nav ul li a");
 
 window.addEventListener("scroll", () => {
   if (scrollY != 0) {
     header.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
     header.style.color = "white";
+    Imag[0].src = "../pictures/reusable-bottle (1).png";
     headerAnchor.forEach((a) => {
       a.style.color = "white";
     });
   } else {
     header.style = "";
+    Imag[0].src = "../pictures/reusable-bottle.png";
     headerAnchor.forEach((a) => {
       a.style.color = "#09777d";
     });
