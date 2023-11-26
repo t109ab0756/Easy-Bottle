fdcea5eff99ea01bae8c7f1e1a7d4f6fe308d90b
謝 <151013528+t109ab0756@users.noreply.github.com>
2023-11-26 17:12:14 +0800

    Update shop.js

--- shop/shop.js
+++ shop/shop.js
@@ -1,21 +1,23 @@
 let header = document.querySelector("header");
+let Imag = document.querySelectorAll("header div.logo img");
 let headerAnchor = document.querySelectorAll("header nav ul li a");
 
 window.addEventListener("scroll", () => {
   if (scrollY != 0) {
     header.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
     header.style.color = "white";
+    Imag[0].src = "../pictures/reusable-bottle (1).png";
     headerAnchor.forEach((a) => {
       a.style.color = "white";
     });
   } else {
     header.style = "";
+    Imag[0].src = "../pictures/reusable-bottle.png";
     headerAnchor.forEach((a) => {
       a.style.color = "#09777d";
     });
   }
 });
-
 function goback() {
   window.location.href = "../index.html";
 }
